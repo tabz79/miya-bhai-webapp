59bd4df67b5f8f60c407eaf791cec6302b8d728e
tabz79 <tabrez.ataher@gmail.com>
2025-10-13 17:53:08 +0530

    home screen grid bug fix-1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,8 @@ module.exports = {
     "grid-cols-3",
     "grid-cols-4",
     "sm:grid-cols-1",
+    "sm:grid-cols-2",
+    "sm:grid-cols-4",
     "md:grid-cols-2",
     "lg:grid-cols-3",
     "xl:grid-cols-4",
