057a3fc37d69ddece9d4b5d0ae8bf8fb93e260b2
tabz79 <tabrez.ataher@gmail.com>
2025-10-13 19:42:05 +0530

    added manual parse and more console logs-1

--- client/src/pages/AuthCallback.tsx
+++ client/src/pages/AuthCallback.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "@/lib/supabaseClient";
+import { Button } from "@/components/ui/button";
 
 export default function AuthCallback() {
   const navigate = useNavigate();
